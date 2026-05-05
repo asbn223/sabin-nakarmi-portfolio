a16a808d9417734acf3f559da998f52eaeb82417
asbn223 <asbn2231@gmail.com>
2026-05-05 21:48:20 +0545

    Finish

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+}
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
 }
 
 export default nextConfig
